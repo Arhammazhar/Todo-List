3da1368312e6e1e06f2d8e4d7f65e77c61d72303
Arhammazhar <arhamatiq2007@gmail.com>
2024-04-02 12:37:08 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoQuestions = await inquirer.prompt([
+        {
+            name: "FirstQuestion",
+            type: "input",
+            message: "What would you like to add in your todos?",
+        },
+        {
+            name: "SecondQuestion",
+            type: "confirm",
+            message: "Would you like to add anything else in your todos?",
+            default: "true",
+        },
+    ]);
+    if (todoQuestions.FirstQuestion != "") {
+        todos.push(todoQuestions.FirstQuestion);
+    }
+    console.log(todos);
+    condition = todoQuestions.SecondQuestion;
+}
+todos.forEach((oneTodo) => console.log(oneTodo));
